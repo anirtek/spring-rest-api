d4f9e84c487b9cff5a39766f5b8be15307302feb
anirtek <atekade1@binghamton.edu>
2018-07-29 17:15:21 -0400

    Spring-rest project: final commit

--- src/main/java/io/anirtek/api/WebConfig.java
+++ src/main/java/io/anirtek/api/WebConfig.java
@@ -2,6 +2,7 @@ package io.anirtek.api;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,4 +18,17 @@ public class WebConfig implements WebMvcConfigurer {
 
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+	
+//	@Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/api/**")
+//            .allowedOrigins("http://mocket.egen.io")
+//            .allowedMethods("PUT", "DELETE", "GET", "POST", "UPDATE", "PATCH", "OPTIONS")
+//            .allowedHeaders("header1", "header2", "header3")
+//            .exposedHeaders("header1", "header2")
+//            .allowCredentials(true).maxAge(3600);
+//
+//        // Add more mappings...
+//    }
 }
